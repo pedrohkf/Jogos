986b310329d0579aa104b76a4d2527bc3797718c
Pedro Henrique <phkfaria@gmail.com>
2023-03-01 18:51:31 -0300

    responsivo

--- asstes/fase3/script3.js
+++ asstes/fase3/script3.js
@@ -3,7 +3,8 @@ const sonic = document.querySelector('.sonic');
 const motog = document.querySelector('.motog');
 const restart = document.querySelector('.restart');
 const moeda = document.querySelector('.moeda');
-const mascote = document.querySelector('.mascote')
+const sonicOver = document.querySelector('.sonic-morto');
+const mascote = document.querySelector('.mascote');
 
 moeda.classList.add('moedas');
 
@@ -11,64 +12,216 @@ moeda.classList.add('moedas');
 
 const jump = () => {
     sonic.classList.add('jump');
-    mascote.classList.add('jump');
 
     setTimeout(() => {
-    sonic.classList.remove('jump');
-    mascote.classList.remove('jump');
+        sonic.classList.remove('jump');
     }, 500);
 
 }
-    const loop = setInterval(() => {
+
+const loop = setInterval(() => {
+
+    const motogPosition = motog.offsetLeft;
+    const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+
+    if (motogPosition <= 110 && motogPosition > 0 && sonicPosition < 170) {
+
+        motog.style.animation = '';
+        motog.style.left = `${motogPosition}px`;
+
+        sonic.style.animation = 'none';
+        sonic.style.bottom = `${motogPosition}px`;
+        sonic.style.display = 'none';
+        sonicOver.style.display = 'flex';
+
+        restart.style.display = 'flex';
+
+        moeda.style.animation = '';
+        moeda.style.animation = 'none';
+    }
+
+}, 10);
+
+function loop2() {
+    setInterval(() => {
 
         const motogPosition = motog.offsetLeft;
-        const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px','' );
-        const mascPosition = +window.getComputedStyle(mascote).bottom.replace('px','' );
+        const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
 
-        if (motogPosition < 110 && motogPosition > 0 && sonicPosition < 160) {
-
-            mascote.style.display = 'none';
+        if (motogPosition <= 100 && motogPosition > 0 && sonicPosition < 220) {
 
             motog.style.animation = '';
             motog.style.left = `${motogPosition}px`;
 
             sonic.style.animation = 'none';
             sonic.style.bottom = `${motogPosition}px`;
-            sonic.src = '/asstes/personagens/sonic game over.png'
-            sonic.style.width = '130px';
-            sonic.style.bottom = '80px';
+            sonic.style.display = 'none';
+            sonicOver.style.display = 'flex';
 
-            restart.src = '/asstes/imgs/restart.png'
-            restart.style.width = '280px';
-            restart.style.left = '530px'
-            restart.style.bottom = '390px'
-            
+            restart.style.display = 'flex';
+
             moeda.style.animation = '';
             moeda.style.animation = 'none';
-            moedas.style.animation = '';
-            moedas.style.animation = 'none';
+        }
+
+    }, 10);
+};
+
+if (window.screen.width < 1285 && window.screen.width > 1025) {
+    loop2();
+}
+
+function loop4() {
+    setInterval(() => {
+
+        const motogPosition = motog.offsetLeft;
+        const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+
+        if (motogPosition <= 100 && motogPosition > 0 && sonicPosition < 270) {
+
+            motog.style.animation = '';
+            motog.style.left = `${motogPosition}px`;
+
+            sonic.style.animation = 'none';
+            sonic.style.bottom = `${motogPosition}px`;
+            sonic.style.display = 'none';
+            sonicOver.style.display = 'flex';
+
+            restart.style.display = 'flex';
+
+            moeda.style.animation = '';
+            moeda.style.animation = 'none';
+            moeda.style.display = 'none';
 
         }
 
     }, 10);
+};
+
+if (window.screen.width < 1025 && window.screen.width > 916) {
+    loop4();
+}
+
+
+function loop3() {
+    const loop3 = setInterval(() => {
+
+        const motogPosition = motog.offsetLeft;
+        const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+
+        if (motogPosition <= 46 && motogPosition > 0 && sonicPosition < 405) {
+
+            motog.style.animation = '';
+            motog.style.left = `${motogPosition}px`;
+
+            sonic.style.animation = 'none';
+            sonic.style.bottom = `${motogPosition}px`;
+            sonic.style.display = 'none';
+            sonicOver.style.display = 'flex';
+
+            restart.style.display = 'flex';
+
+            moeda.style.animation = '';
+            moeda.style.animation = 'none';
+            moeda.style.display = 'none';
+        }
+
+    }, 10);
+}
+
+if (window.screen.width < 916 && window.screen.width > 845) {
+    loop3();
+}
+
+function loop5() {
+    const loop3 = setInterval(() => {
+
+        const motogPosition = motog.offsetLeft;
+        const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+
+        if (motogPosition <= 45 && motogPosition > 0 && sonicPosition < 306) {
+
+            motog.style.animation = '';
+            motog.style.left = `${motogPosition}px`;
+
+            sonic.style.animation = 'none';
+            sonic.style.bottom = `${motogPosition}px`;
+            sonic.style.display = 'none';
+            sonicOver.style.display = 'flex';
+
+            restart.style.display = 'flex';
+
+            moeda.style.animation = '';
+            moeda.style.animation = 'none';
+            moeda.style.display = 'none';
+        }
+
+    }, 10);
+}
+
+if (window.screen.width < 845) {
+    loop5();
+}
+
+function loop7() {
+    const loop5 = setInterval(() => {
+
+        const motogPosition = motog.offsetLeft;
+        const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+
+        if (motogPosition <= 45 && motogPosition > 0 && sonicPosition < 355) {
+
+            motog.style.animation = '';
+            motog.style.left = `${motogPosition}px`;
+
+            sonic.style.animation = 'none';
+            sonic.style.bottom = `${motogPosition}px`;
+            sonic.style.display = 'none';
+            sonicOver.style.display = 'flex';
+
+            restart.style.display = 'flex';
+
+            moeda.style.animation = '';
+            moeda.style.animation = 'none';
+            moeda.style.display = 'none';
+        }
+
+    }, 10);
+}
+
+if (window.screen.width < 668) {
+    loop7();
+}
+
+function loop8() {
+    const loop5 = setInterval(() => {
+
+        const motogPosition = motog.offsetLeft;
+        const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+
+        if (motogPosition <= 45 && motogPosition > 0 && sonicPosition < 365) {
+
+            motog.style.animation = '';
+            motog.style.left = `${motogPosition}px`;
+
+            sonic.style.animation = 'none';
+            sonic.style.bottom = `${motogPosition}px`;
+            sonic.style.display = 'none';
+            sonicOver.style.display = 'flex';
+
+            restart.style.display = 'flex';
+
+            moeda.style.animation = '';
+            moeda.style.animation = 'none';
+            moeda.style.display = 'none';
+        }
+
+    }, 10);
+}
+
+if (window.screen.width > 668 && window.screen.width < 745) {
+    loop8();
+}
 
 document.addEventListener('keydown', jump);
-
-
-/*etInterval(() => {
-    moeda.style.width = '70px'
-    moeda.style.bottom = '103px';hjvchjcvjvjvjghgjfv
-
-}, 3000);*/
-
-
-
-
-
-
-
-
-
-
-
- 
+document.addEventListener('click', jump);
--- index2.js
+++ index2.js
@@ -9,6 +9,6 @@ setTimeout(function(){
 
 setTimeout(function jogar() {
     div.style.backgroundColor = 'rgb(109, 109, 109)';
-    inicio.style.display = 'block';
+    inicio.style.display = 'flex';
     intro.style.display= 'none';
 }, 12000);
